4a4379b2999b56e19f51afff7f5989cf2e3f7571
cruisecg <cruisecg.cg@gmail.com>
2020-09-18 18:17:55 +0800

    modify - add IDParseTime() IDParseStep()  IDParseNode()

--- idGenerator.go
+++ idGenerator.go
@@ -5,6 +5,25 @@ import (
 	"github.com/pkg/errors"
 )
 
+const (
+	// Epoch is set to the twitter snowflake epoch of Nov 04 2010 01:42:54 UTC in milliseconds
+	// You may customize this to set a different epoch for your application.
+	Epoch int64 = 1288834974657
+
+	// NodeBits holds the number of bits to use for Node
+	// Remember, you have a total 22 bits to share between Node/Step
+	NodeBits uint8 = 10
+
+	// StepBits holds the number of bits to use for Step
+	// Remember, you have a total 22 bits to share between Node/Step
+	StepBits  uint8 = 12
+	nodeMax   int64 = -1 ^ (-1 << NodeBits)
+	nodeMask        = nodeMax << StepBits
+	stepMask  int64 = -1 ^ (-1 << StepBits)
+	timeShift       = NodeBits + StepBits
+	nodeShift       = StepBits
+)
+
 type idGenerator struct {
 	gen *snowflake.Node
 }
@@ -17,8 +36,8 @@ type idGenerator struct {
 // to be generated every millisecond, per Node ID.
 func newIDGenerator(nodeNum int64) (*idGenerator, error) {
 
-	if nodeNum<0 || nodeNum>1023{
-		return nil,errors.New("node out of range")
+	if nodeNum < 0 || nodeNum > 1023 {
+		return nil, errors.New("node out of range")
 	}
 	node, err := snowflake.NewNode(nodeNum)
 
@@ -36,4 +55,19 @@ func newIDGenerator(nodeNum int64) (*idGenerator, error) {
 func (g *idGenerator) GenerateInt64ID() int64 {
 
 	return g.gen.Generate().Int64()
+}
+
+// Time returns an int64 unix timestamp in milliseconds of the snowflake ID time
+func IDParseTime(id int64) int64 {
+	return (id >> timeShift) + Epoch
+}
+
+// Node returns an int64 of the snowflake ID node number
+func IDParseNode(id int64) int64 {
+	return id & nodeMask >> nodeShift
+}
+
+// Step returns an int64 of the snowflake step (or sequence) number
+func IDParseStep(id int64) int64 {
+	return id & stepMask
 }
--- idGenerator_test.go
+++ idGenerator_test.go
@@ -7,18 +7,59 @@ import (
 
 func Test_newIDGenerator(t *testing.T) {
 
-
-	g,err:=newIDGenerator(1)
-	if err!=nil{
-		t.Fatalf("err %s",err.Error())
+	g, err := newIDGenerator(1)
+	if err != nil {
+		t.Fatalf("err %s", err.Error())
 	}
-	id:= g.GenerateInt64ID()
+	id := g.GenerateInt64ID()
 
-	if reflect.TypeOf(id).String()!="int64"{
-		t.Fatalf("type error got %s want %s\n",reflect.TypeOf(id),"int64")
+	if reflect.TypeOf(id).String() != "int64" {
+		t.Fatalf("type error got %s want %s\n", reflect.TypeOf(id), "int64")
 	}
 
+}
 
+func TestParseTime(t *testing.T) {
 
+	//node 0
+	var node0 int64 =0
+	g0, _ := newIDGenerator(node0)
 
+	var node99 int64 =99
+	g99, _ := newIDGenerator(node99)
+
+	var node1023 int64 =1023
+	g1023, _ := newIDGenerator(node1023)
+
+	type args struct {
+		id int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			"0",
+			args{g0.GenerateInt64ID()},
+			node0,
+		},
+		{
+			"1",
+			args{g99.GenerateInt64ID()},
+			node99,
+		},
+		{
+			"0",
+			args{g1023.GenerateInt64ID()},
+			node1023,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IDParseNode(tt.args.id); got != tt.want {
+				t.Errorf("ParseNode() = %d, want %d", got, tt.want)
+			}
+		})
+	}
 }
