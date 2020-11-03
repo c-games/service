bcf414e039da6307d90ca7f0a174b9140b9dd2e8
jerryChang <jerryChang.cg@gmail.com>
2020-11-03 12:43:50 +0800

    fix  - fix compound keys is empty string

--- dbutil/dbutil.go
+++ dbutil/dbutil.go
@@ -77,11 +77,11 @@ func GenCreateTable(s interface{}) string {
 		compoundIndexName, ok := f.Tag.Lookup("compound_index")
 		if ok {
 			fmt.Printf("compoundIndexName: %s\n", compoundIndexName)
-			if key, ok := compoundIndex[compoundIndexName]; ok {
+			if _, ok := compoundIndex[compoundIndexName]; ok {
 				compoundIndex[compoundIndexName] = append(compoundIndex[compoundIndexName], name)
 				fmt.Printf("compoundIndex: %+v\n", compoundIndex)
 			} else {
-				compoundKeys = append(key)
+				compoundKeys = append(compoundKeys, compoundIndexName)
 				compoundIndex[compoundIndexName] = append(compoundIndex[compoundIndexName], name)
 				fmt.Printf("keys: %s\n", compoundKeys)
 				fmt.Printf("compoundIndex: %+v\n", compoundIndex)
